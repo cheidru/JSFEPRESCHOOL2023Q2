e450c7c336e5f3759353521740c22686c864cf18
My Name <my@test.com>
2023-08-13 14:08:36 +0300

    feat: complete burger menu functionality

--- library/burger-menu.js
+++ library/burger-menu.js
@@ -1,1 +1,35 @@
-const burgerButton = document.getElementById('burger-button');
+const burgerButton = document.querySelector('.burger-button');
+const anyWhere = document.querySelector('body');
+const navBurgerMenu = document.querySelector('.nav-ul');
+const profile = document.getElementById('profile-svg');
+
+navBurgerMenu.classList.add('nav-burger-menu-invisible');
+console.log('program started');
+
+burgerButton.addEventListener('click', (e) => {
+    console.log('burger button function enabled');
+    e.stopImmediatePropagation();
+    menuShowHide();
+})
+
+navBurgerMenu.addEventListener('click', (e) => {
+    e.stopImmediatePropagation();
+    menuShowHide();
+})
+
+profile.addEventListener('click', (e) => {
+    e.stopImmediatePropagation();
+})
+
+anyWhere.addEventListener('click', (e) => {
+    if (burgerButton.classList.contains('cross-button')) {
+        menuShowHide();
+    }
+})
+
+function menuShowHide() {
+    burgerButton.classList.toggle('burger-button');
+    burgerButton.classList.toggle('cross-button');
+    navBurgerMenu.classList.toggle('nav-burger-menu-visible');
+    navBurgerMenu.classList.toggle('nav-burger-menu-invisible');
+}
