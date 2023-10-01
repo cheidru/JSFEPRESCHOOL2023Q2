c22469c1c0d316bc0715a20bfbcfc841f501150e
Igor Checheurov <chei@yandex.ru>
2023-10-01 21:36:02 +0300

    feat: add splash window

--- image-galery/index.js
+++ image-galery/index.js
@@ -5,79 +5,33 @@ const searchField = document.getElementById('search-field');
 const frame = document.querySelector('body');
 const searchBTN = document.getElementById('magni-glass');
 const cancelBTN = document.getElementById('blank-field');
+const splashBOX = document.getElementById('splashBox');
+const powerLayer = document.getElementById('power-layer');
+
 
 const unsplashURL = "https://api.unsplash.com/photos/?client_id=kohkTo9ZcV19ZIATEKoz3NcmhVAUERsr5At0ENH2GQk&per_page=16";
-let searchString = "query=image"
+let searchString = "query=image";
 
 let windowGotLoaded = false;
 let searchEnabled = false;
+let regularIMG = [];
 
 //  в запросе ?qwery=image&per_page=16 должно дать 16
 // по тегу image Выдост фото на разные темы
 
-const altLocalImage = [
-    "a man standing next to a tent in the desert",
-    "a lone tree in the middle of a field at night",
-    "a scenic view of the ocean and a road",
-    "a table topped with figs and flowers on top of a cloth",
-    "a path through a grove of trees in a forest",
-    "a group of men standing next to each other",
-    "a road lined with lots of tall bamboo trees",
-    "a woman in a white dress and hat standing in tall grass",
-    "an aerial view of a road and a river",
-    "two old cars sitting in a field with the aurora in the background"
-];
-
-const localSmallImageSRC = [
-    "./assets/img/small/photo-0-small.jpg",
-    "./assets/img/small/photo-1-small.jpg",
-    "./assets/img/small/photo-2-small.jpg",
-    "./assets/img/small/photo-3-small.jpg",
-    "./assets/img/small/photo-4-small.jpg",
-    "./assets/img/small/photo-5-small.jpg",
-    "./assets/img/small/photo-6-small.jpg",
-    "./assets/img/small/photo-7-small.jpg",
-    "./assets/img/small/photo-8-small.jpg",
-    "./assets/img/small/photo-9-small.jpg",
-    "./assets/img/small/photo-10-small.jpg",
-    "./assets/img/small/photo-11-small.jpg",
-    "./assets/img/small/photo-12-small.jpg",
-    "./assets/img/small/photo-13-small.jpg",
-    "./assets/img/small/photo-14-small.jpg",
-    "./assets/img/small/photo-15-small.jpg"
-];
-
-const localThumbImageSRC = [
-    "./assets/img/thumb/photo-0-thumb.jpg",
-    "./assets/img/thumb/photo-1-thumb.jpg",
-    "./assets/img/thumb/photo-2-thumb.jpg",
-    "./assets/img/thumb/photo-3-thumb.jpg",
-    "./assets/img/thumb/photo-4-thumb.jpg",
-    "./assets/img/thumb/photo-5-thumb.jpg",
-    "./assets/img/thumb/photo-6-thumb.jpg",
-    "./assets/img/thumb/photo-7-thumb.jpg",
-    "./assets/img/thumb/photo-8-thumb.jpg",
-    "./assets/img/thumb/photo-9-thumb.jpg",
-    "./assets/img/thumb/photo-10-thumb.jpg",
-    "./assets/img/thumb/photo-11-thumb.jpg",
-    "./assets/img/thumb/photo-12-thumb.jpg",
-    "./assets/img/thumb/photo-13-thumb.jpg",
-    "./assets/img/thumb/photo-14-thumb.jpg",
-    "./assets/img/thumb/photo-15-thumb.jpg"
-];
-
 async function loadImage() {
     try {
         const response = await fetch(unsplashURL + '&' + searchString);
+        console.log('request string =', unsplashURL + '&' + searchString);
         const data = await response.json();
         console.log(data);
-        dispensePictures(data);
+        renderImg(data, resizeGalleryWrapper);
     } catch (error) {
         console.log(error);
     }
 }
 
-function loadLocalImage() {
+function resizeGalleryWrapper() {
     galleryWrapper.style.height = imageDistribution() + 'px';
 }
 
@@ -91,23 +45,19 @@ function imageDistribution() {
     return (totalHeight / 4) * 1.2
 }
 
-function renderLocalImg(foo) {
+function renderImg(data, foo) {
     for(let i = 0; i < 16; i++) {
-        galleryCell[i].src = localThumbImageSRC[i];
+        galleryCell[i].src = data[i].urls.thumb;
+        regularIMG[i] = data[i].urls.regular;
+        // galleryCell[i].src = localThumbImageSRC[i];
     }
     lastPicture.onload = () => foo();
-}
-
-function dispensePictures(data) {
-    for(let i = 0; i < 16; i++) {
-        galleryCell[i].src  = data[i].urls.thumb;
-    }
 }
 
 window.addEventListener('load', () => {
     if(!windowGotLoaded) {
     searchField.focus();
-    renderLocalImg(loadLocalImage);
+    loadImage();
     windowGotLoaded = true;
     }
 })
@@ -129,7 +79,40 @@ searchField.addEventListener('input', () => {
     }
 })
 
+searchField.addEventListener('keydown', (event)=> {
+    if(event.code == 'Enter') sendRequest();
+})
+
+searchBTN.addEventListener('click', () => {
+    sendRequest();
+})
+
+function sendRequest() {
+    console.log('request sent');
+    searchString = "query=" + searchField.value;
+    console.log('searchField.value =', searchField.value);
+    loadImage();
+}
+
 cancelBTN.addEventListener('click', () => {
     searchField.value = '';   
     cancelBTN.style.display = 'none'; 
 })
+
+galleryWrapper.addEventListener('click', (event) => {
+    if(event.target.tagName == 'IMG') {
+        console.log(event.target.id, powerLayer.style, splashBOX.style);
+        splashBOX.src = regularIMG[Number(event.target.id.substr(5))];
+        powerLayer.style.display = 'flex';
+        splashBOX.style.display = 'block';
+        frame.style.overflow = 'hidden';
+    }
+})
+
+powerLayer.addEventListener('click', () => {
+    powerLayer.style.display = 'none';
+    splashBOX.style.display = 'none';
+    frame.style.overflow = 'auto'
+})
+
+
