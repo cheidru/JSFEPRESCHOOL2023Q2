6916fa0d355cabb291c57fe5343231283caf5198
Igor Checheurov <chei@yandex.ru>
2023-10-06 01:23:29 +0300

    feat: add some more movement

--- random-game/index.js
+++ random-game/index.js
@@ -1,11 +1,12 @@
 const pad = document.getElementById('launch-pad');
 const ball = document.getElementById('ball');
 const playField = document.getElementById('field');
+const coord = document.getElementById('coordinates');
 
 let padPosition = {};
 padPosition.startX = pad.getBoundingClientRect().x;
 padPosition.actualX = pad.getBoundingClientRect().x;
-padPosition.endX = pad.getBoundingClientRect().x - 40 - pad.getBoundingClientRect().width;
+padPosition.endX = playField.getBoundingClientRect().x + playField.getBoundingClientRect().width - 40 - pad.getBoundingClientRect().width;
 
 // function dragPad() {
 //     console.log('stopPropagation');
@@ -26,14 +27,19 @@ padPosition.endX = pad.getBoundingClientRect().x - 40 - pad.getBoundingClientRec
             console.log('pointer move');
                 let lineRightEnd = padPosition.endX;
                 let startPosition = padPosition.startX;
+                let offset = pad.getBoundingClientRect().width / 2;
     
                 if (event.pageX < startPosition) {
                     pad.style.transform = 'translateX(0px)';
                 } else if (event.pageX > lineRightEnd) {                    
                     pad.style.transform = `translateX(${lineRightEnd}px)`
+                    console.log('pad.style.transform =', pad.style.transform);
                 } else {
-                    pad.style.transform = `translateX(${event.pageX - startPosition}px)`;
+                    pad.style.transform = `translateX(${event.pageX - startPosition - offset}px)`;
+                    console.log('pad X', pad.getBoundingClientRect().x);
                 }
+                coord.textContent = pad.getBoundingClientRect().x;
+                console.log('event.pageX =', event.pageX, 'lineRightEnd =', lineRightEnd, 'startPosition =', startPosition);
         }
 
         pad.onpointerup = () => {
