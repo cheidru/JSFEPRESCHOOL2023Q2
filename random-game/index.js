c9d51a48176af882af66081781fedcf301e92234
Чечеуров Игорь Дмитриевич <ichecheurov@stan-group.com>
2023-10-05 17:05:36 +0300

    feat: start programming pad movement

--- random-game/index.js
+++ random-game/index.js
@@ -1,15 +1,44 @@
 const pad = document.getElementById('launch-pad');
 const ball = document.getElementById('ball');
-let padStyle = getComputedStyle(pad);
-let pasOriginX = padStyle.left;
+const playField = document.getElementById('field');
 
+let padPosition = {};
+padPosition.startX = pad.getBoundingClientRect().x;
+padPosition.actualX = pad.getBoundingClientRect().x;
+padPosition.endX = pad.getBoundingClientRect().x - 40 - pad.getBoundingClientRect().width;
 
-pad.addEventListener('pointerdown', (e) => {
-    dragPad(e)
-})
+// function dragPad() {
+//     console.log('stopPropagation');
 
-function dragPad(event) {
-    event.stopPropagation();    
-    padStyle.cursor = "none";
-    padStyle.left = (parseInt(pasOriginX) + 30) + 'px';
-}
+    // pad.ondragstart = () => false;
+    // pad.style.transform = `translateX(${padPosition.startX}px)`;
+
+    // Listeners to control player thumb position when it is changed manually
+    pad.onpointerdown = function(event) {
+    console.log('pointer down');
+        // prevent selection start (browser action)
+        // event.preventDefault();
+
+        // начать отслеживание перемещения указателя и переопределить их на ползунок
+        pad.setPointerCapture(event.pointerId);
+
+        pad.onpointermove = function(event) {
+            console.log('pointer move');
+                let lineRightEnd = padPosition.endX;
+                let startPosition = padPosition.startX;
+    
+                if (event.pageX < startPosition) {
+                    pad.style.transform = 'translateX(0px)';
+                } else if (event.pageX > lineRightEnd) {                    
+                    pad.style.transform = `translateX(${lineRightEnd}px)`
+                } else {
+                    pad.style.transform = `translateX(${event.pageX - startPosition}px)`;
+                }
+        }
+
+        pad.onpointerup = () => {
+            pad.onpointermove = null;
+            pad.onpointerup = null;
+        }
+    }
+// }
