8d8a26476aaa209a606d2ed7103405c2eb75064d
Чечеуров Игорь Дмитриевич <ichecheurov@stan-group.com>
2023-10-05 14:35:19 +0300

    feat: add pad moving pattern

--- random-game/index.js
+++ random-game/index.js
@@ -1,3 +1,15 @@
 const pad = document.getElementById('launch-pad');
 const ball = document.getElementById('ball');
+let padStyle = getComputedStyle(pad);
+let pasOriginX = padStyle.left;
 
+
+pad.addEventListener('pointerdown', (e) => {
+    dragPad(e)
+})
+
+function dragPad(event) {
+    event.stopPropagation();    
+    padStyle.cursor = "none";
+    padStyle.left = (parseInt(pasOriginX) + 30) + 'px';
+}
